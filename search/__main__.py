1e7e3d41b515f3bcd4f68fb6eaa8cca8578872da
Juwey <juweytan@gmail.com>
2020-03-18 17:01:00 +1100

    Read input from file and format and print

--- search/__main__.py
+++ search/__main__.py
@@ -1,12 +1,25 @@
 import sys
 import json
 
-from search.util import print_move, print_boom, print_board
+from util import print_move, print_boom, print_board
 
 
 def main():
     with open(sys.argv[1]) as file:
         data = json.load(file)
+        black_data = data["black"]
+        board_dict = {}
+        for stack in black_data:
+            coords = tuple(stack[1:])
+            board_dict[coords] = "B"
+        
+        white_data = data["white"]
+
+        for stack in white_data:
+            coords = tuple(stack[1:])
+            board_dict[coords] = "W"
+
+        print_board(board_dict)
 
     # TODO: find and print winning action sequence
 
